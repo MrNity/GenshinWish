377f3a7950c6eb1fd80de3c1044ede928aaf6b75
MrNity <nitayl@list.ru>
2022-01-08 00:39:47 +0300

    Initial commit

---
+++ GenshinWish/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace GenshinWish
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCopy.Location = new System.Drawing.Point(12, 56);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(261, 72);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "СКОПИРОВАТЬ ССЫЛКУ КРУТОК";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Для начала нужно прогрузить историю круток в геншине!";
+            // 
+            // lblVer
+            // 
+            this.lblVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblVer.Location = new System.Drawing.Point(126, 131);
+            this.lblVer.Name = "lblVer";
+            this.lblVer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblVer.Size = new System.Drawing.Size(147, 25);
+            this.lblVer.TabIndex = 3;
+            this.lblVer.Text = "v0.1";
+            this.lblVer.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(285, 165);
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Genshin Круточки";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVer;
+    }
+}
+
---
+++ GenshinWish/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenshinWish
+{
+    public partial class Form1 : Form
+    {
+        string version = "v0.1";
+        public Form1()
+        {
+            InitializeComponent();
+            lblVer.Text = version;
+        }
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string fileName = @"%USERPROFILE%\AppData\LocalLow\miHoYo\Genshin Impact\output_log.txt";
+            String file = Environment.ExpandEnvironmentVariables(fileName);
+
+            FileInfo log = new FileInfo(file);
+            using (var sr = new StreamReader(log.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                string text = sr.ReadToEnd();
+                string findLink = @"OnGetWebViewPageFinish:https://webstatic-sea.mihoyo.com/";
+                string findLog = @"#/log";
+
+                int lenLink = findLink.Length;
+                try
+                {
+                    string link = text.Substring(text.IndexOf(findLink), text.IndexOf(findLog) - text.IndexOf(findLink) + findLog.Length + findLink.Length);
+                    link = link.Replace("OnGetWebViewPageFinish:", "");
+                    link = link.Substring(0, link.IndexOf(findLog) + findLog.Length);
+                    Clipboard.SetText(link);
+                    MessageBox.Show("Ссылка скопирована!");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Ссылки нет!");
+                }
+                
+            }
+        }
+
+    }
+}
